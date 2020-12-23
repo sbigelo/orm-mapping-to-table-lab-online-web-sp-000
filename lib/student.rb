595289d5dc5a874a254a6fd6cf37ada371d911a2
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-23 20:52:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,15 @@
 class Student
-attr_accessor :name, :grade
+attr_accessor :name, :grade, :id
 
-  def initialize(name, grade)
+  def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
+    @id = id
   end
 
+  def self.create_table
 
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
