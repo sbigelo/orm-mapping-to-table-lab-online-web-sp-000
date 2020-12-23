2ee3a99d399a08835b3d68fcd840af242c818dc6
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-23 21:11:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,6 +29,9 @@ attr_reader :id
     VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+
   end
 
   # Remember, you can access your database connection anywhere in this class
