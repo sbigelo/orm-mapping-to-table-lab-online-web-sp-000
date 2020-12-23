e8939e42bb04702b4f2c90c42a2a14bdc10f6551
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-23 20:56:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,13 @@ attr_reader :id
   end
 
   def self.create_table
-
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students
+      (id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER)
+      SQL
+      DB[:conn].execute(sql)
   end
 
   # Remember, you can access your database connection anywhere in this class
